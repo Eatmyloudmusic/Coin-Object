7bc0938aed951a894cbcd21f55bcb1d174eab62b
william_michael_brough <eatmyloudmusic@gmail.com>
2020-06-25 20:10:06 -0400

    working 001

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ console.log('fucky')
 
 const heads = document.createElement("div")
 let front = document.createElement("img")
-front.src = "./assets/sonic.jpg";
-front.height = 220;
-front.width = 220;
-heads.append(front)
-mainElement.append(heads)
+// front.src = "./heads.png";
+// front.height = 220;
+// front.width = 220;
+// heads.append(front)
+// mainElement.append(heads)
 
 
 
@@ -22,7 +22,7 @@ let coin = {
 
     flip: function () {
         this.state = Math.round(Math.random() * 1);
-        
+        console.log(this.state)
 
 
 
@@ -31,19 +31,51 @@ let coin = {
     },
     toString: function () {
         coin.flip()
+        let image = document.createElement('img');
+
         if (this.state === 0) {
-            console.log("Heads")
-            document.body.append("go home")
+            document.body.append("heads")
+            image.src = 'heads.png'
+          document.body.append(image)
         } else {
             document.body.append("tails")
+            image.src ='tails.png'
+          document.body.append(image)
         }
         /* 2. Return the string "Heads" or "Tails", depending on whether
            "this.state" is 0 or 1. */
     },
-    toHTML: function () {
-        coin.flip()
+    // toHTML: function () {
+    //     coin.flip()
 
 
-        let image = document.createElement('img');
-      
-        /* 3. Set the
+    //     let image = document.createElement('img');
+    //   if (this.state === 0){
+    //       image.src = 'heads.png'
+    //       document.body.append(image)
+        
+    //   }
+    //   else {
+    //       image.src ='tails.png'
+    //       document.body.append(image)
+    //   }
+    // }
+}
+
+//        /* 3. Set the
+
+// document.body.append(image)
+
+
+
+function check(){
+    coin.toString()
+    // coin.toHTML()
+}
+
+check()
+// function display20Flips()
+//     for (i=0; i< display20Flips.length; i ++20 )
+
+// function display20Images()
+//     for (i=0; i< display20Images.length; i ++20 )
